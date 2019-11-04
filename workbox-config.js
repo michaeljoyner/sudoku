1ef209043dc8664aab77a9b9ba47db41ad2273eb
Michael Joyner <joyner.michael@gmail.com>
2019-11-04 11:15:35 +0800

    wip

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "globDirectory": "dist/",
-  "globPatterns": [
-    "**/*.{css,ico,html,js,svg,webmanifest}"
-  ],
-  "swDest": "dist/sw.js",
-  "swSrc": "public/service-worker.js"
-};
